274123be140e7d656a66229ab08ce95d6df771ae
Weiheng Liao <weiheng_liao@outlook.com>
2023-03-06 16:57:37 +0800

    2023-03-06 Update by Level1: 1. Some aesthetic changes

--- patpat-viewer/viewer.py
+++ patpat-viewer/viewer.py
@@ -1,4 +1,4 @@
-"""test
+"""
 
     Typical usage example:
     典型用法示例：
@@ -42,79 +42,19 @@ def home():
     return render_template('Home.html')
 
 
-"""
-@app.route('/search', methods=['GET', 'POST'])
-def search():
-
-    if request.method == 'POST':
-        identifier = request.form.get('identifier')
-        q = hub.QueryHub()
-        protein_id = identifier
-        q.identifier = protein_id
-        q.simple_query()
-
-        return redirect(url_for('taskbar'), code=301)
-        # redirect(url_for('search_loading', identifier=identifier), code=301)
-
-    return render_template('Search.html')
-"""
-
-
 @patpat_viewer.route('/taskbar', methods=['GET', 'POST'])
-def taskbar(pagination_num_per=5):
+def taskbar(pagination_num_per=10):
     configs = utility.config_process()
 
     pagination_num_per = pagination_num_per
 
     configs_group = utility.group_list(configs, pagination_num_per)
-    """
-    
-    pagination_num = range(1, len(configs_group) + 1)
 
-    if re.search("(?<=\\?p).*", request.url):
-        page = int(re.search("(?<=\\?p).*", request.url).group())
-    else:
-        page = 1
-
-    this_page_configs = configs_group[page - 1]
-    """
     this_page_data, pagination_num, page = choose_page(groups=configs_group)
     return render_template('Taskbar.html',
                            configs=this_page_data,
                            pagination_num=pagination_num,
                            page=page)
-
-
-def choose_page(groups):
-    pagination_num = range(1, len(groups) + 1)
-
-    if re.search("(?<=\\?p).*", request.url):
-        page = int(re.search("(?<=\\?p).*", request.url).group())
-    else:
-        page = 1
-
-    this_page_data = groups[page - 1]
-    return this_page_data, pagination_num, page
-
-
-@patpat_viewer.errorhandler(404)
-def page_not_found():
-    return render_template('404.html'), 404
-
-
-@patpat_viewer.route('/about')
-def about():
-    return render_template('About.html')
-
-
-@patpat_viewer.route('/contact')
-def contact():
-    return render_template('Contact.html')
-
-
-@patpat_viewer.route('/test')
-def test():
-    return render_template('test.html')
 
 
 @patpat_viewer.route('/taskbar/<uid>', methods=['GET'])
@@ -160,6 +100,55 @@ def test2(
     else:
         return render_template('Empty.html')
 
+
+@patpat_viewer.route('/about')
+def about():
+    return render_template('About.html')
+
+
+@patpat_viewer.route('/contact')
+def contact():
+    return render_template('Contact.html')
+
+
+@patpat_viewer.errorhandler(404)
+def page_not_found():
+    return render_template('404.html'), 404
+
+
+@patpat_viewer.route('/test')
+def test():
+    return render_template('test.html')
+
+
+def choose_page(groups):
+    pagination_num = range(1, len(groups) + 1)
+
+    if re.search("(?<=\\?p).*", request.url):
+        page = int(re.search("(?<=\\?p).*", request.url).group())
+    else:
+        page = 1
+
+    this_page_data = groups[page - 1]
+    return this_page_data, pagination_num, page
+
+
+"""
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+
+    if request.method == 'POST':
+        identifier = request.form.get('identifier')
+        q = hub.QueryHub()
+        protein_id = identifier
+        q.identifier = protein_id
+        q.simple_query()
+
+        return redirect(url_for('taskbar'), code=301)
+        # redirect(url_for('search_loading', identifier=identifier), code=301)
+
+    return render_template('Search.html')
+"""
 
 if __name__ == '__main__':
     patpat_viewer.run(debug=True)
